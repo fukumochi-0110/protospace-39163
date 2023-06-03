cd38f5ef2c38b3f54c826a5ea73b272fe2b100ca
fukumochi-0110 <fukumoto08@gmail.com>
2023-06-04 01:50:16 +0900

    Update prototypes_controller.rb

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :set_prototype, only: [:show, :edit]
+  before_action :set_prototype, only: [:edit]
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
 
   def index
